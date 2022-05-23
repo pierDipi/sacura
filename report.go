4c362d2b69a39717a6134cc027630983810f37ea
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-05-23 14:09:26 +0200

    Introduce duplicate threshold (#18)
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- report.go
+++ report.go
@@ -13,6 +13,13 @@ type Metrics struct {
 type Report struct {
 	LostCount                int                 `json:"lostCount"`
 	LostEventsByPartitionKey map[string][]string `json:"lostEvents"`
-	Terminated               bool                `json:"terminated"`
-	Metrics                  Metrics             `json:"metrics"`
+	DuplicateCount           int                 `json:"duplicateCount"`
+	// DuplicateEventsByPartitionKey collects duplicate events by
+	DuplicateEventsByPartitionKey map[string][]string `json:"duplicateEvents"`
+	// ReceivedCount is the number of events received, including duplicates
+	ReceivedCount int `json:"receivedCount"`
+	// ReceivedEventsByPartitionKey collects all events by partition, including duplicates
+	ReceivedEventsByPartitionKey map[string][]string `json:"-"`
+	Terminated                   bool                `json:"terminated"`
+	Metrics                      Metrics             `json:"metrics"`
 }
