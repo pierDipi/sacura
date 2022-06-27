d4d272867430b7c677fb4fc45ab4e885b7a90e61
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-06-27 12:55:21 +0200

    WIP
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/pierdipi/sacura
 go 1.14
 
 require (
-	github.com/cloudevents/sdk-go/v2 v2.3.1
+	github.com/cloudevents/sdk-go/v2 v2.10.1
 	github.com/dgryski/go-gk v0.0.0-20200319235926-a69029f61654 // indirect
+	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96 // indirect
+	github.com/go-openapi/spec v0.0.0-20160808142527-6aced65f8501 // indirect
 	github.com/go-yaml/yaml v2.1.0+incompatible
 	github.com/google/go-cmp v0.5.2
 	github.com/google/uuid v1.1.1
 	github.com/influxdata/tdigest v0.0.1 // indirect
+	github.com/lightstep/tracecontext.go v0.0.0-20181129014701-1757c391b1ac // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/onsi/gomega v1.10.1 // indirect
 	github.com/tsenart/vegeta/v12 v12.8.3
@@ -16,11 +19,10 @@ require (
 	go.uber.org/zap v1.15.0 // indirect
 	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6 // indirect
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
-	golang.org/x/net v0.0.0-20200930145003-4acb6c075d10 // indirect
-	golang.org/x/sys v0.0.0-20200905004654-be1d3432aa8f // indirect
-	golang.org/x/tools v0.0.0-20201002161817-08f19738fac6 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
 	gonum.org/v1/netlib v0.0.0-20190331212654-76723241ea4e // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
 	honnef.co/go/tools v0.0.1-2020.1.5 // indirect
-	k8s.io/apimachinery v0.19.2
+	k8s.io/apimachinery v0.24.2
 )
+
+replace github.com/tsenart/vegeta/v12 => github.com/tsenart/vegeta/v12 v12.8.3
--- receiver.go
+++ receiver.go
@@ -5,14 +5,61 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	ce "github.com/cloudevents/sdk-go/v2"
 	ceclient "github.com/cloudevents/sdk-go/v2/client"
 	cehttp "github.com/cloudevents/sdk-go/v2/protocol/http"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/metric/global"
+	"go.opentelemetry.io/otel/metric/instrument"
+	"go.opentelemetry.io/otel/metric/instrument/syncint64"
+	"go.opentelemetry.io/otel/metric/unit"
+	"go.opentelemetry.io/otel/sdk/metric/aggregator/histogram"
+	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
+	"go.opentelemetry.io/otel/sdk/metric/export/aggregation"
+	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
+	selector "go.opentelemetry.io/otel/sdk/metric/selector/simple"
 )
 
+var (
+	latencyHistogram       syncint64.Histogram  = nil
+	latencyHistogramLabels []attribute.KeyValue = nil
+)
+
+const (
+	BenchmarkTimestampAttribute = "ce-benchmarktimestamp"
+)
+
+func init() {
+	meter := global.MeterProvider().Meter("sacura")
+
+	keyValueLabels := strings.Split(os.Getenv("LATENCY_E2E_METRIC_LABELS"), ",")
+	labels := make([]attribute.KeyValue, 0, len(keyValueLabels)*2)
+	for _, kv := range keyValueLabels {
+		parts := strings.Split(kv, "=")
+		labels = append(labels, attribute.String(parts[0], parts[1]))
+	}
+	latencyHistogramLabels = labels
+
+	var err error
+	latencyHistogram, err = meter.SyncInt64().Histogram("latency_e2e",
+		instrument.WithUnit(unit.Milliseconds),
+		instrument.WithDescription("Histogram for E2E latency since "+BenchmarkTimestampAttribute),
+	)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func StartReceiver(ctx context.Context, config ReceiverConfig, received chan<- ce.Event) error {
+	exportMetrics(ctx)
+
 	defer close(received)
 
 	protocol, err := cehttp.New(cehttp.WithPort(config.Port))
@@ -38,6 +85,17 @@ func StartReceiver(ctx context.Context, config ReceiverConfig, received chan<- c
 	}()
 
 	err = client.StartReceiver(innerCtx, func(ctx context.Context, event ce.Event) {
+		exstensions := event.Extensions()
+		if v, ok := exstensions[BenchmarkTimestampAttribute]; ok {
+			t, err := strconv.ParseInt(v.(string), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			start := time.Unix(t, 0)
+			latency := time.Since(start)
+			latencyHistogram.Record(ctx, int64(latency), latencyHistogramLabels...)
+		}
+
 		maybeSleep(config)
 		received <- event
 	})
@@ -64,4 +122,39 @@ func maybeSleep(config ReceiverConfig) {
 	min := *config.ReceiverFaultConfig.MinSleepDuration
 
 	time.Sleep(min + time.Duration(rand.Int63n(int64(max-min))))
+}
+
+func exportMetrics(ctx context.Context) {
+	config := prometheus.Config{}
+
+	ctrl := controller.New(
+		processor.NewFactory(
+			selector.NewWithHistogramDistribution(
+				histogram.WithExplicitBoundaries(config.DefaultHistogramBoundaries),
+			),
+			aggregation.CumulativeTemporalitySelector(),
+			processor.WithMemory(true),
+		),
+	)
+
+	exporter, err := prometheus.New(config, ctrl)
+	if err != nil {
+		panic(err)
+	}
+
+	global.SetMeterProvider(exporter.MeterProvider())
+
+	go func() {
+		s := http.Server{
+			Handler: http.HandlerFunc(exporter.ServeHTTP),
+		}
+		go func() {
+			if err := s.ListenAndServe(); err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		<-ctx.Done()
+		_ = s.Close()
+	}()
 }
